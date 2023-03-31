9e20a679c6707591590ea86bba748c2951278c04
kklinka <kklinka@gmail.com>
2023-03-31 18:17:42 +0200

    dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,71 @@
-﻿string name = "Kasia";
-string gender = "kobieta";
-int age = 29;
+﻿int number = 4594;
 
-if (gender == "kobieta" && age < 30)
+string numberInString = number.ToString();
+
+char[] letters = numberInString.ToArray();
+
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+int counter0 = 0;
+
+foreach (char letter in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}  
-else if (name == "Ewa" && age == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter =='1') 
+    { 
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++; 
+    }
 }
-else if (gender == "mężczyzna" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
+
+Console.WriteLine("Cyfry 0 jest " + counter0);
+Console.WriteLine("Cyfry 1 jest " + counter1);
+Console.WriteLine("Cyfry 2 jest " + counter2);
+Console.WriteLine("Cyfry 3 jest " + counter3);
+Console.WriteLine("Cyfry 4 jest " + counter4);
+Console.WriteLine("Cyfry 5 jest " + counter5);
+Console.WriteLine("Cyfry 6 jest " + counter6);
+Console.WriteLine("Cyfry 7 jest " + counter7);
+Console.WriteLine("Cyfry 8 jest " + counter8);
+Console.WriteLine("Cyfry 9 jest " + counter9);
